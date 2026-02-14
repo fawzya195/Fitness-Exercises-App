468777d874999b3337d00451dbd355ece539143f
Fawzya Said <fawzya195@users.noreply.github.com>
2026-02-14 12:59:15 +0200

    intilize  Fitness Exercises App With APIs

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      fooetr
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Pages/ExerciseExamble.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExerciseExamble() {
+  return (
+    <div>
+      ex
+    </div>
+  )
+}
+
+export default ExerciseExamble
